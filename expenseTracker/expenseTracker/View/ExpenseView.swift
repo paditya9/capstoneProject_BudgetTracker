57662e4e684261be6f3fe476281f904974f7a966
Adityaraj Padmanabhan <148919991+paditya9@users.noreply.github.com>
2024-04-19 13:22:12 -0400

    Added Code files

---
+++ expenseTracker/expenseTracker/View/ExpenseView.swift
@@ -0,0 +1,106 @@
+//
+//  ExpenseView.swift
+//  expenseTracker
+//
+//  Created by Adityaraj - CodePath on 4/13/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ExpenseView: View {
+    @Binding var currentScreen: String
+    @Query(sort: [SortDescriptor(\Expense.date, order: .reverse)], animation: .smooth)
+    private var allExpenses: [Expense]
+    @Environment(\.modelContext) private var context
+    @State private var groupingExpenses: [GroupingExpense] = []
+    @State private var addExpense: Bool = false
+    
+    
+    var body: some View {
+        NavigationStack{
+            List{
+                ForEach($groupingExpenses){ $group in
+                    Section(group.groupTitle) {
+                        ForEach(group.expenses) { expense in
+                            // Box view
+                            BoxView(expense: expense)
+                                .swipeActions(edge: .trailing, allowsFullSwipe: false){
+                                    Button{
+                                        context.delete(expense)
+                                        withAnimation{
+                                            group.expenses.removeAll(where: { $0.id == expense.id})
+                                            if group.expenses.isEmpty{
+                                                groupingExpenses.removeAll(where: { $0.id == group.id})
+                                            }
+                                        }
+                                    }
+                                label: {
+                                    Image(systemName: "trash.fill")
+                                }
+                                .tint(.red)
+                                    
+                                }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Expense Dashboard")
+            .overlay{ if allExpenses.isEmpty || groupingExpenses.isEmpty{
+                ContentUnavailableView("No Expense Recorded", systemImage: "tray")
+            }}
+            .toolbar{
+                ToolbarItem(placement: .topBarTrailing){
+                    Button{
+                        addExpense.toggle()
+                    }
+                label: {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.title2)
+                }
+                }
+            }
+        }
+        .onChange(of: allExpenses, initial: true){ oldValue, newValue in
+            if newValue.count > oldValue.count || groupingExpenses.isEmpty || currentScreen == "Categories"{
+                makeGroupExpense(newValue)
+            }
+        }
+        .sheet(isPresented: $addExpense){
+            AddExpenseView()
+                .interactiveDismissDisabled()
+        }
+    }
+    
+    func makeGroupExpense(_ expenses: [Expense]){
+        Task.detached(priority: .high){
+            let group = Dictionary(grouping: expenses) { expenses in
+                let dataComp = Calendar.current.dateComponents([.day, .month, .year], from: expenses.date)
+                
+                return dataComp
+                
+            }
+            let sortDict = group.sorted{
+                let calendar = Calendar.current
+                let date1 = calendar.date(from: $0.key) ?? .init()
+                let date2 = calendar.date(from: $1.key) ?? .init()
+                
+                return calendar.compare(date1, to: date2, toGranularity: .day) == .orderedDescending
+            }
+            
+            await MainActor.run{
+                groupingExpenses = sortDict.compactMap({ dict in
+                    let date = Calendar.current.date(from: dict.key) ?? .init()
+                    return .init(date: date, expenses: dict.value)
+                    
+                })
+                
+            }
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ expenseTracker/expenseTrackerUITests/expenseTrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  expenseTrackerUITestsLaunchTests.swift
+//  expenseTrackerUITests
+//
+//  Created by Adityaraj - CodePath on 4/13/24.
+//
+
+import XCTest
+
+final class expenseTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
